51f84145b00494fd63210be76deea9ae67e9320e
Sleeeee <jstasjustin@gmail.com>
2024-10-21 17:59:01 +0200

    Add interface overview switching behavior

--- src/interface/gui.py
+++ src/interface/gui.py
@@ -14,10 +14,11 @@ class ParkEaseApp:
         self.logo_frame = LogoFrame(self.root)
         self.title_frame = TitleFrame(self.root)
         self.banner_frame = BannerFrame(self.root)
-        self.sidebar_frame = SidebarFrame(self.root)
-        self.current_mainframe = ParkingOverviewFrame(self.root)
+        self.sidebar_frame = SidebarFrame(self.root, self)
+        self.current_mainframe = None
     
         self.init_grid()
+        self.switch_mainframe(ParkingOverviewFrame)
 
     def init_grid(self):
         """Initializes the grid layout"""
@@ -31,9 +32,13 @@ class ParkEaseApp:
         """Creates the style for the child frames to use"""
         style = ttk.Style()
         style.configure("Default.TFrame", margin=5, padding=5, borderwidth=1, relief="solid", background="white")
+        style.configure("Default.TButton", background="white", borderwidth=1)
+        style.configure("Default.TLabel", background="white")
 
     def switch_mainframe(self, frame_class):
         """Switches the main frame between the different available views"""
+        if isinstance(self.current_mainframe, frame_class):
+            return
         if self.current_mainframe is not None:
             self.current_mainframe.destroy() # Frees memory for the current frame
         # TODO check for values and raise ValueErrors
@@ -59,9 +64,14 @@ class BannerFrame(ttk.Frame):
 
 class SidebarFrame(ttk.Frame):
     """Frame containing the buttons to switch the current overview"""
-    def __init__(self, parent):
+    def __init__(self, parent, app):
         super().__init__(parent, style="Default.TFrame")
         self.grid(row=2, column=0, sticky="nsew", padx=10, pady=10)
+
+        ttk.Button(self, text="Manage parkings", style="Default.TButton", command=lambda: app.switch_mainframe(ParkingOverviewFrame)).pack()
+        ttk.Button(self, text="Manage payments", style="Default.TButton", command=lambda: app.switch_mainframe(PaymentsOverviewFrame)).pack()
+        ttk.Button(self, text="Manage subscribers", style="Default.TButton", command=lambda: app.switch_mainframe(SubscribersOverviewFrame)).pack()
+        ttk.Button(self, text="View analytics", style="Default.TButton", command=lambda: app.switch_mainframe(AnalyticsOverviewFrame)).pack()
 
 class MainFrame(ttk.Frame):
     """Parent class to the overviews"""
@@ -73,20 +83,23 @@ class ParkingOverviewFrame(MainFrame):
     """Frame used to view the parking lot occupancy"""
     def __init__(self, parent):
         super().__init__(parent)
-        label = ttk.Label(self, text="Hello world !")
-        label.pack()
+        ttk.Label(self, text="I love parkings !", style="Default.TLabel").pack()
 
 class PaymentsOverviewFrame(MainFrame):
     """Frame used to encode or review payments"""
     def __init__(self, parent):
-        super().__init__(parent) 
+        super().__init__(parent)
+        ttk.Label(self, text="I love payments !", style="Default.TLabel").pack()
 
 class SubscribersOverviewFrame(MainFrame):
     """Frame used to manage subscribers"""
     def __init__(self, parent):
         super().__init__(parent)
+        ttk.Label(self, text="I love subscribers !", style="Default.TLabel").pack()
 
 class AnalyticsOverviewFrame(MainFrame):
     """Frame used to visualize current or past data about the parking lots and generate reports"""
     def __init__(self, parent):
         super().__init__(parent)
+        ttk.Label(self, text="I love analytics !", style="Default.TLabel").pack()
+
