092ade999072b0479b54b4388fd03dc03b1aa44d
Sleeeee <jstasjustin@gmail.com>
2024-11-27 19:30:00 +0100

    Enhance models.py readability

--- src/models.py
+++ src/models.py
@@ -2,20 +2,32 @@ from typing import Dict
 
 class ParkingSpot:
     """
-    A class representing a parking spot. The ParkingSpot constructor is only called within ParkingRow (eventually while creating a top-level Parking object)
+    A class representing a parking spot.
+    The ParkingSpot constructor is only called within ParkingRow
+    (eventually while creating a top-level Parking object)
 
     Attributes :
-        - spot_number (read-only int) : The number of the spot inside its container ParkingRow
-        - status (str) : Real-time status of the spot. Must be one of the following :
-            - free : The spot is currently free and customers can book or be placed in that spot
-            - occupied : Someone is currently occuppying the spot, it can't be booked, noone can be placed in that spot
-            - booked : Someone is meant to park in that spot. Only that person can be placed in that spot
-        - _booking_id (read-only str | None) : The id of the current booking linked to the spot. None if the spot is not booked
+        - spot_number (read-only int) :
+          The number of the spot inside its container ParkingRow
+        - status (str) : Real-time status of the spot.
+          Must be one of the following :
+            - free : The spot is currently free and customers can booked
+              or be placed in that spot
+            - occupied : Someone is currently occuppying the spot,
+              it can't be booked, noone can be placed in that spot
+            - booked : Someone is meant to park in that spot.
+              Only that person can be placed in that spot
+        - _booking_id (read-only str | None) : The id of the current booking
+          linked to the spot. None if the spot is not booked
     """
 
     ALLOWED_STATUSES = ["free", "occupied", "booked"] 
 
-    def __init__(self, id: int, spot_number: int, status: str = "free", booking_id: int = None):
+    def __init__(self, id: int,
+                 spot_number: int,
+                 status: str = "free"):
+                 #booking_id: int = None):
+        """"""
         self._id = id
         self._spot_number = spot_number
         self.status = status # Forces usage of @status.setter
@@ -23,26 +35,33 @@ class ParkingSpot:
 
     @property
     def id(self):
+        """"""
         return self._id
 
     @property
     def spot_number(self):
+        """"""
         return self._spot_number
 
     @property
     def status(self):
+        """"""
         return self._status
 
     @status.setter
     def status(self, status: str):
+        """"""
         if status not in self.ALLOWED_STATUSES:
-            raise ValueError(f"Invalid status '{status}'. Must be one the following : {self.ALLOWED_STATUSES}")
+            raise ValueError(f"""Invalid status '{status}'.
+            Must be one the following : {self.ALLOWED_STATUSES}""")
         self._status = status
 
     def __str__(self):
+        """"""
         string = f"Spot {self.spot_number} : {self.status}"
         if self.status != "free":
-            string += f" by {self.linked_car.registration_plate}" # Adds the registration plate if the spot is occupied or booked
+            # Adds the registration plate if the spot is occupied or booked
+            string += f" by {self.linked_car.registration_plate}"
         return string
 
     def enter(self, registration_plate: str):
@@ -77,46 +96,57 @@ class ParkingSpot:
         """
         self.status = "booked"
         #self._booking_id = booking_id
-        pass
+        return registration_plate
 
 class ParkingRow:
+    """"""
     def __init__(self, row_number: int):
+        """"""
         self._row_number = row_number
         self.spots = {}
 
     @property
     def row_number(self):
+        """"""
         return self._row_number
 
     def __str__(self):
+        """"""
         output = [f"Row {self.row_number}"]
         for spot in sorted(self.spots):
             output.append(f"            {str(self.spots[spot])}")
         return "\n".join(output)
 
     def add_spot(self, spot: Dict[str, int]):
+        """"""
         #spot.keys() = "id", "spot_number"
         self.spots[spot["spot_number"]] = ParkingSpot(spot["id"], spot["spot_number"])
 
     def remove_spot(self, spot_number : int):
+        """"""
         del self.spots[spot_number]
 
 class ParkingFloor:
+    """"""
     def __init__(self, floor_number: int):
+        """"""
         self._floor_number = floor_number
         self.rows = {}
 
     @property
     def floor_number(self):
+        """"""
         return self._floor_number
 
     def __str__(self):
+        """"""
         output = [f"Floor {self.floor_number}"]
         for row in sorted(self.rows):
             output.append(f"        {str(self.rows[row])}")
         return "\n".join(output)
 
     def add_spot(self, spot: Dict[str, int]):
+        """"""
         #spot.keys() = "id", "spot_number", "row_number"
         id, spot_number, row_number = spot["id"], spot["spot_number"], spot["row_number"]
         if row_number not in self.rows:
@@ -124,25 +154,31 @@ class ParkingFloor:
         self.rows[row_number].add_spot({"id": id, "spot_number": spot_number})
 
     def remove_spot(self, spot):
+        """"""
         spot_number, row_number = spot["spot_number"], spot["row_number"]
         self.rows[row_number].remove_spot(spot_number)
         if not self.rows[row_number].spots:
             del self.rows[row_number]
 
 class ParkingLot:
+    """"""
     def __init__(self, lot_number: int):
         """
         PRE : lot_number est un entier
-        POST : l'objet ParkingLot possède un numéro d'identification et un dictionnaire vide contenant les étages (qui contiendront les rangées, qui elles contiendront les emplacements)
+        POST : l'objet ParkingLot possède un numéro d'identification
+        et un dictionnaire vide contenant les étages
+        (qui contiendront les rangées, qui elles contiendront les emplacements)
         """
         self._lot_number = lot_number
         self.floors = {}
 
     @property
     def lot_number(self):
+        """"""
         return self._lot_number
 
     def __str__(self):
+        """"""
         output = [f"Parking {self.lot_number}"]
         for floor in sorted(self.floors):
             output.append(f"    {str(self.floors[floor])}")
@@ -150,15 +186,24 @@ class ParkingLot:
 
     def add_spot(self, spot: Dict[str, int]):
         """"
-        PRE : Spot est un dictionnaire dont les clés sont "id", "spot_number", "row_number", "floor_number", chacune correspondant à un entier
-        POST : Si l'étage demandé n'existe pas, il est créé (à vide). Appelle ensuite la méthode add_spot() de l'objet ParkingFloor, qui va créer un ParkingRow si besoin. À terme, L'objet ParkingSpot est créé dans le ParkingRow correspondant
-        RAISES : KeyError si l'emplacement existe déjà / TypeError si spot n'est pas un dictionnaire ou ne correspond pas à la description indiquée
+        PRE : Spot est un dictionnaire dont les clés sont
+              "id", "spot_number", "row_number", "floor_number",
+              chacune correspondant à un entier
+        POST : Si l'étage demandé n'existe pas, il est créé (à vide).
+               Appelle ensuite la méthode add_spot() de l'objet ParkingFloor,
+               qui va créer un ParkingRow si besoin.
+               À terme, L'objet ParkingSpot est créé dans le ParkingRow correspondant
+        RAISES : KeyError si l'emplacement existe déjà,
+                 TypeError si spot n'est pas un dictionnaire
+                 ou ne correspond pas à la description indiquée
         """
         # TODO : finir la vérification des types et raise l'erreur correspondante
         id, spot_number, row_number, floor_number = spot["id"], spot["spot_number"], spot["row_number"], spot["floor_number"] 
         if floor_number not in self.floors:
             self.floors[floor_number] = ParkingFloor(floor_number)
-        self.floors[floor_number].add_spot({"id": id, "spot_number": spot_number, "row_number": row_number})
+        self.floors[floor_number].add_spot({"id": id, 
+                                            "spot_number": spot_number,
+                                            "row_number": row_number})
 
     def remove_spot(self, spot):
         """
@@ -174,21 +219,28 @@ class ParkingLot:
         
 
 class Car:
+    """"""
     def __init__(self, registration_plate):
+        """"""
         self.registration_plate = registration_plate
 
 class StandardCar(Car):
+    """"""
     HOURLY_RATE = 3.00
 
 class PremiumCar(Car):
+    """"""
     HOURLY_RATE = 2.0
 
 class Payment:
+    """"""
     def __init__(self, usage_id, registration_plate, amount):
+        """""""
         self.usage_id = usage_id
         self.registration_plate = registration_plate
         self.amount = amount
 
     def __str__(self):
+        """"""
         return f"#{self.usage_id} - {self.registration_plate} paid {self.amount}€"
 
