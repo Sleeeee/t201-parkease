d7453aa073bcad25694204e6754a9639e770f3c7
Sleeeee <jstasjustin@gmail.com>
2024-11-28 06:34:44 +0100

    Add basic test files and running them on push

---
+++ unit_tests/test_models.py
@@ -0,0 +1,21 @@
+import unittest
+
+class TestParkingSpot(unittest.TestCase):
+    def test_init(self):
+        self.assertTrue(True)
+
+class TestParkingRow(unittest.TestCase):
+    def test_init(self):
+        self.assertTrue(True)
+
+class TestParkingFloor(unittest.TestCase):
+    def test_init(self):
+        self.assertTrue(True)
+
+class TestParkingLot(unittest.TestCase):
+    def test_init(self):
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
+
---
+++ unit_tests/test_parking_controller.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestParkingController(unittest.TestCase):
+    def test_init(self):
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
